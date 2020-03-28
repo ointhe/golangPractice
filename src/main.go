6a80772a47e195fae95ca6ff69ec0a9346bbb359
Factory Is <oisfactory@gmail.com>
2020-03-28 17:29:13 +0900

    Test code first

--- src/main.go
+++ src/main.go
@@ -1,1 +1,38 @@
-go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticle(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Contents"},
+	}
+
+	fmt.Println("Test json")
+	json.NewEncoder(w).Encode(articles)
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Test Homepage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/article", allArticle)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequests()
+}
